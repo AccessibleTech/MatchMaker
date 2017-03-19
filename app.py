92ad76021ef17bc84bc6e1156573e590f751a196
Florian Rathgeber <florian.rathgeber@gmail.com>
2017-03-19 17:18:10 +0000

    Add app

---
+++ app.py
@@ -0,0 +1,23 @@
+from os import environ
+import time
+
+from gattlib import DiscoveryService
+from upm import pyupm_grove as grove
+
+led = grove.GroveLed(int(environ.get('LED_COLOR', 2)))
+
+service = DiscoveryService("hci0")
+
+while True:
+    devices = service.discover(5)
+
+    for address, name in devices.items():
+        print("name: {}, address: {}".format(name, address))
+        if name.startswith('edison'):
+            # Turn the LED on and off 10 times, pausing 1/4 second
+            # between transitions
+            for i in range(0, 10):
+                led.on()
+                time.sleep(.25)
+                led.off()
+                time.sleep(.25)
