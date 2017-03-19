e26029c9d30248015d806ebbd211c4669096ba29
Florian Rathgeber <florian.rathgeber@gmail.com>
2017-03-19 17:29:35 +0000

    only log Edison

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ while True:
     devices = service.discover(5)
 
     for address, name in devices.items():
-        print("name: {}, address: {}".format(name, address))
         if name.startswith('edison') or address.startswith('58:A8:39:00'):
+            print("Edison detected - name: {}, address: {}".format(name, address))
             # Turn the LED on and off 10 times, pausing 1/4 second
             # between transitions
             for i in range(0, 10):
