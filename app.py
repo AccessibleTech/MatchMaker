7d65cadba9b6349cc95d9623767f73129f8f0737
Florian Rathgeber <florian.rathgeber@gmail.com>
2017-03-19 17:26:04 +0000

    Name not properly detected

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from upm import pyupm_grove as grove
 
 led = grove.GroveLed(int(environ.get('LED_COLOR', 2)))
 
+time.sleep(.25)
 service = DiscoveryService("hci0")
 
 while True:
@@ -13,7 +14,7 @@ while True:
 
     for address, name in devices.items():
         print("name: {}, address: {}".format(name, address))
-        if name.startswith('edison'):
+        if name.startswith('edison') or address.startswith('58:A8:39:00'):
             # Turn the LED on and off 10 times, pausing 1/4 second
             # between transitions
             for i in range(0, 10):
